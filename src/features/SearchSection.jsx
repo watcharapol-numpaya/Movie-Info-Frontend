75b93577b43ae553ed2aa6ca4b3a331057c9ea23
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-05 00:21:14 +0700

    [UPDATE] search movie complete

--- src/features/SearchSection.jsx
+++ src/features/SearchSection.jsx
@@ -1,27 +1,46 @@
-import React, { useState } from "react";
-
+import React, { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMovieByKeyword } from "../storage/slides/movieSlice";
 import SearchCard from "../components/SearchCard";
 
 function SearchSection() {
   const [keyword, setKeyword] = useState("");
+  const [showSearchCard, setShowSearchCard] = useState(false);
+  const inputRef = useRef(null);
   const dispatch = useDispatch();
   const { searchList } = useSelector((state) => state.movies);
   const limitedData = searchList.slice(0, 5);
 
   const handleSearch = (e) => {
-    setKeyword(e.target.value.trim());
     dispatch(getMovieByKeyword(e.target.value.trim()));
+    setShowSearchCard(true); // Show the search card when typing in the input box
   };
 
   const handleIsShowSearchMovieCard = () => {
     return limitedData.length !== 0;
   };
 
+  const handleInputClick = () => {
+    setShowSearchCard(true);
+  };
+
+  const handleClickOutsideInput = (e) => {
+    if (inputRef.current && !inputRef.current.contains(e.target)) {
+      setShowSearchCard(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutsideInput);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutsideInput);
+    };
+  }, []);
+
   const renderSearchMovieCard = () => {
     return (
-      <div className="bg-white text-black  w-80 rounded-lg overflow-hidden  shadow-2xl">
+      <div className="bg-white text-black w-80 rounded-lg overflow-hidden shadow-2xl">
         {limitedData &&
           limitedData.map((movie) => (
             <SearchCard key={movie.id} movie={movie} />
@@ -35,26 +54,31 @@ function SearchSection() {
 
   return (
     <>
-      <div className="   h-full items-center  w-full pl-4 relative  ">
-        <div className="relative  h-full w-full flex items-center justify-center    ">
-          <div className="absolute flex items-center justify-center bg-yellow-400 rounded-full h-12 w-12 left-3 cursor-pointer ">
+      <div className="h-full items-center w-full pl-4 relative">
+        <div className="relative h-full w-full flex items-center justify-center">
+          <div className="absolute flex items-center justify-center bg-yellow-400 rounded-full h-12 w-12 left-3 cursor-pointer">
             <span className="material-icons -scale-x-90 text-black text-3xl">
               search
             </span>
           </div>
           <input
+            ref={inputRef}
+            onClick={handleInputClick}
             onChange={handleSearch}
+ 
             placeholder="Search movie"
-            className="outline-none text-black  sm:w-72 w-56  py-1 text-xl px-1 pl-5 rounded-r-lg border-2 border-yellow-400"
+            className="outline-none text-black sm:w-72 w-56 py-1 text-xl px-1 pl-5 rounded-r-lg border-2 border-yellow-400"
             type="text"
           />
         </div>
 
-        <div id="search-card" className="absolute z-10 top-14 left-8  ">
-          {/* -----Show-Movie-card------ */}
-          {handleIsShowSearchMovieCard() ? renderSearchMovieCard() : ""}
-          {/* ------------ */}
-        </div>
+        {showSearchCard && (
+          <div id="search-card" className="absolute z-10 top-14 left-8">
+            {/* -----Show-Movie-card------ */}
+            {handleIsShowSearchMovieCard() ? renderSearchMovieCard() : ""}
+            {/* ------------ */}
+          </div>
+        )}
       </div>
     </>
   );
