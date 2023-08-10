b2c0098406ae98d744af5bb3daf98d06137f0348
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-10 16:25:00 +0700

    [UPDATE] start handle prop

--- src/pages/MovieInfoPage.jsx
+++ src/pages/MovieInfoPage.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect } from "react";
-import {useLocation, useParams} from "react-router-dom"
-function MovieInfoPage({  }) {
-    const location = useLocation()
-    const {movie} = location.state
-    const {id} = useParams()
+import { useDispatch } from "react-redux";
+import { useLocation, useParams } from "react-router-dom";
+function MovieInfoPage({}) {
+  const location = useLocation();
+  const movie = location.state?.movie;
+  const { id } = useParams();
+  const dispatch = useDispatch();
 
-    useEffect(()=>{},[])
-
+  useEffect(() => {
+    if (!movie) {
+      console.log("NOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    }else{
+      dispatch(get)
+    }
+  }, []);
 
   return (
     <>
-      <div>MovieInformationPage : {id} 
-      {console.log(movie)}
-      {movie.title}
-      <div>--------------</div>
-      {console.log(location)}
+      <div>
+        MovieInformationPage : {id}
+        {console.log(movie)}
+        {/* {movie.title} */}
+        <div>--------------</div>
+        {/* {console.log(movie.movie.title)} */}
       </div>
     </>
   );
