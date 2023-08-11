551fa86359bd2985ded649b6c19de52f3d8d5995
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-12 01:00:46 +0700

    [UPDATE] banner section of info page

--- src/pages/MovieInfoPage.jsx
+++ src/pages/MovieInfoPage.jsx
@@ -5,7 +5,6 @@ import { getMovieByID } from "../storage/slides/movieSlice";
 import OnLoadingScreen from "../components/OnLoadingScreen";
 
 const MovieInfoPage = ({}) => {
-
   const { movieInfo } = useSelector((state) => state.movies);
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
@@ -17,19 +16,78 @@ const MovieInfoPage = ({}) => {
     dispatch(getMovieByID(id)).then(() => {
       setIsLoading(false);
     });
- 
   }, [id]);
 
   const renderMovieInfo = () => {
     return (
       <>
-        <div className="lg:container mx-auto bg-blue-400">
-          {" "}
-          <div>MovieInformationPage : {id}</div>
-          <div>MovieInformationPage : {id}</div>
-          <div>MovieInformationPage : {id}</div>
-          <div className="w-96">
-            <img src={`${imageUrl}/${movieInfo.poster_path}`} />
+        <div className=" h-full w-full ">
+          <div id="banner-section" className="h-128 w-full bg-gray-400">
+            <div className="xl:container mx-auto  w-full flex justify-center ">
+              <div className="relative  bg-blue-400 h-128 w-full   ">
+                <div id="image-background" className="h-128 w-full opacity-40">
+                  <img
+                    className="w-full h-full object-cover"
+                    src={`${imageUrl}/${movieInfo.backdrop_path}`}
+                    alt="Banner Image"
+                  />
+                </div>
+                <div className="absolute top-0 h-full w-full bg-fuchsia-300 flex ">
+                  <div className=" bg-red-200 h-full w-2/6">
+                    <div className="w-full h-full flex justify-center items-center">
+                      <img
+                        className="h-112 "
+                        src={`${imageUrl}/${movieInfo.poster_path}`}
+                      />
+                    </div>
+                  </div>
+                  <div className=" bg-red-400 h-full w-4/6">
+                    <div className=" p-8 bg-blue-200">
+                      <p className="text-white text-3xl font-semibold">
+                        {movieInfo.title}
+                      </p>
+                    </div>
+                    <div className="px-8 text-white">
+                      <p className="text-xl font-bold">Overview</p>
+                      <p className=" text-lg font-normal">
+                        {movieInfo.overview}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const renderMovieInfoMobile = () => {
+    return (
+      <>
+        <div className="xl:container mx-auto  ">
+          <div className="w-full h-full   flex items-center justify-center">
+            <div className=" relative h-full w-full flex justify-center  ">
+              <div id="background-image" className="opacity-70">
+                <img
+                  className=" "
+                  src={`${imageUrl}/${movieInfo.backdrop_path}`}
+                />
+              </div>
+
+              <div className="absolute w-full h-full flex items-center">
+                <div className="pl-2">
+                  <img
+                    className=" w-32 "
+                    src={`${imageUrl}/${movieInfo.poster_path}`}
+                  />
+                </div>
+                <div className="  h-full pt-3 p-1 text-white">
+                  <p className="font-semibold">{movieInfo.title}</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
@@ -39,7 +97,7 @@ const MovieInfoPage = ({}) => {
   return (
     <>
       {console.log(movieInfo)}
-      <div className="bg-blue-200 w-full h-full ">
+      <div className="  w-full h-full ">
         {isLoading ? <OnLoadingScreen /> : renderMovieInfo()}
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -12,12 +11,24 @@ export default {
       height: {
         86: "21rem",
         95: "23rem",
+        112: "28rem",
         128: "32rem",
+        144: "36rem",
+        160: "160rem",
+        176: "44rem",
+        192: "52rem",
       },
       width: {
         "70%": "70%",
         "30%": "30%",
         86: "21rem",
+        95: "23rem",
+        112: "28rem",
+        128: "32rem",
+        144: "36rem",
+        160: "160rem",
+        176: "44rem",
+        192: "52rem",
       },
       letterSpacing: {
         "widest0.25": "0.25em",
