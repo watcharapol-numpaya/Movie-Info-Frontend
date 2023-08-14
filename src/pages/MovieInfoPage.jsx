8c44890be8a35e821d0081449e4c3b49884c3579
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-15 00:21:53 +0700

    [ADD] add prepare responsive mobile

--- src/pages/MovieInfoPage.jsx
+++ src/pages/MovieInfoPage.jsx
@@ -28,77 +28,87 @@ const MovieInfoPage = ({}) => {
     return (
       <>
         <div className=" h-full w-full ">
-          <div id="banner-section" className="h-128 w-full   ">
-            <div className="xl:container mx-auto  w-full flex justify-center ">
-              <div className="relative   h-128 w-full   bg-black">
-                <div id="image-background" className="h-128 w-full opacity-40 ">
+          <div id="banner-section" className="h-full w-full   ">
+            {renderTitleAndBannerSectionMobile()}
+          </div>
+
+          <div id="details" className=" ">
+            {renderSecondSection()}
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  const renderTitleAndBannerSection = () => {
+    return (
+      <>
+        <div className="xl:container mx-auto  w-full flex justify-center ">
+          <div className="relative   h-128 w-full   bg-black ">
+            <div id="image-background" className="h-128 w-full opacity-40 ">
+              <img
+                className="w-full h-full object-cover"
+                src={`${imageUrl}/${movieInfo.backdrop_path}`}
+                alt="Banner Image"
+              />
+            </div>
+            <div className="absolute top-0 h-full w-full  flex  ">
+              <div id="poster" className="  h-full w-2/6">
+                <div className="w-full h-full flex justify-center items-center   ">
                   <img
-                    className="w-full h-full object-cover"
-                    src={`${imageUrl}/${movieInfo.backdrop_path}`}
-                    alt="Banner Image"
+                    className="lg:h-112 md:h-96 sm:h-80 h-72 lg:w-76 md:w-64 sm:w-56 w-48 shadow-xl rounded-xl border-4 border-white bg-gray-200"
+                    src={`${imageUrl}/${movieInfo.poster_path}`}
                   />
                 </div>
-                <div className="absolute top-0 h-full w-full  flex  ">
-                  <div id="poster" className="  h-full w-2/6">
-                    <div className="w-full h-full flex justify-center items-center   ">
-                      <img  
-                        className="lg:h-112 md:h-96 sm:h-80 h-72 lg:w-76 md:w-64 sm:w-56 w-48 shadow-xl rounded-xl border-4 border-white bg-gray-200"
-                        src={`${imageUrl}/${movieInfo.poster_path}`}
-                      />
-                    </div>
+              </div>
+              <div
+                id="content"
+                className="pl-4 pr-2 lg:pt-10 md:pt-16 sm:pt-24 pt-28  h-full w-4/6   "
+              >
+                <div className="   ">
+                  <p className="text-white md:text-4xl sm:text-3xl text-2xl font-semibold  ">
+                    {movieInfo.title}
+                  </p>
+                </div>
+                <hr className="border my-2 mt-3" />
+                <div className="text-white  flex flex-wrap  ">
+                  {/* <span className="font-semibold">Genres :</span> */}
+                  {movieInfo.genres.map((genre, index) => (
+                    <span
+                      className="md:text-base sm:text-sm text-xs bg-yellow-500 ml-1 rounded-lg p-1 cursor-pointer"
+                      key={genre.id}
+                    >
+                      {genre.name}
+                      {/* {index !== movieInfo.genres.length - 1 && ","} */}
+                    </span>
+                  ))}
+                </div>
+                <div className="w-full mt-2 flex items-center">
+                  <CircularProgressBar percentage={voteAvgInPercentage} />
+                  <div className="mx-1">
+                    <button className="bg-black p-2 border-2 border-white text-white font-medium ">
+                      Play Trailer
+                    </button>
                   </div>
-                  <div
-                    id="content"
-                    className="pl-4 pr-2 lg:pt-10 md:pt-16 sm:pt-24 pt-28  h-full w-4/6   "
-                  >
-                    <div className="   ">
-                      <p className="text-white md:text-4xl sm:text-3xl text-2xl font-semibold  ">
-                        {movieInfo.title}
-                      </p>
-                    </div>
-                    <hr className="border my-2 mt-3" />
-                    <div className="text-white  flex flex-wrap  ">
-                      {/* <span className="font-semibold">Genres :</span> */}
-                      {movieInfo.genres.map((genre, index) => (
-                        <span
-                          className="md:text-base sm:text-sm text-xs bg-yellow-500 ml-1 rounded-lg p-1 cursor-pointer"
-                          key={genre.id}
-                        >
-                          {genre.name}
-                          {/* {index !== movieInfo.genres.length - 1 && ","} */}
-                        </span>
-                      ))}
-                    </div>
-                    <div className="w-full mt-2 flex items-center">
-                      <CircularProgressBar percentage={voteAvgInPercentage} />
-                      <div className="mx-1">
-                        <button className="bg-black p-2 border-2 border-white text-white font-medium ">
-                          Play Trailer
-                        </button>
-                      </div>
-                    </div>
+                </div>
 
-                    <div id="overview" className=" text-white    ">
-                      <p className=" md:text-2xl sm:text-xl text-lg font-bold py-2   ">
-                        Overview
-                      </p>
-                      <p
-                        className={`${
-                          movieInfo.overview.length >= 1380
-                            ? "overflow-y-scroll "
-                            : ""
-                        } lg:h-64 md:h-48 sm:h-40 h-36   md:text-lg sm:text-base text-sm font-normal overflow-auto    `}
-                      >
-                        {movieInfo.overview}
-                      </p>
-                    </div>
-                  </div>
+                <div id="overview" className=" text-white    ">
+                  <p className=" md:text-2xl sm:text-xl text-lg font-bold py-2   ">
+                    Overview
+                  </p>
+                  <p
+                    className={`${
+                      movieInfo.overview.length >= 1380
+                        ? "overflow-y-scroll "
+                        : ""
+                    } lg:h-64 md:h-48 sm:h-40 h-36   md:text-lg sm:text-base text-sm font-normal overflow-auto    `}
+                  >
+                    {movieInfo.overview}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-
-          <div className=" ">{renderSecondSection()}</div>
         </div>
       </>
     );
@@ -149,28 +159,71 @@ const MovieInfoPage = ({}) => {
     );
   };
 
-  const renderMovieInfoMobile = () => {
+  const renderTitleAndBannerSectionMobile = () => {
     return (
       <>
-        <div className="xl:container mx-auto  ">
-          <div className="w-full h-full   flex items-center justify-center">
-            <div className=" relative h-full w-full flex justify-center  ">
-              <div id="background-image" className="opacity-70">
-                <img
-                  className="  "
-                  src={`${imageUrl}/${movieInfo.backdrop_path}`}
-                />
-              </div>
-
-              <div className="absolute w-full h-full flex items-center">
-                <div className="pl-2 ">
+          <div className="xl:container mx-auto  w-full flex justify-center ">
+          <div className="relative   h-128 w-full   bg-black ">
+            <div id="image-background" className="h-128 w-full opacity-40 ">
+              <img
+                className="w-full h-full object-cover"
+                src={`${imageUrl}/${movieInfo.backdrop_path}`}
+                alt="Banner Image"
+              />
+            </div>
+            <div className="absolute top-0 h-full w-full  flex  ">
+              <div id="poster" className="  h-full w-2/6">
+                <div className="w-full h-full flex justify-center items-center   ">
                   <img
-                    className=" w-32 "
+                    className="lg:h-112 md:h-96 sm:h-80 h-72 lg:w-76 md:w-64 sm:w-56 w-48 shadow-xl rounded-xl border-4 border-white bg-gray-200"
                     src={`${imageUrl}/${movieInfo.poster_path}`}
                   />
                 </div>
-                <div className="  h-full pt-3 p-1 text-white">
-                  <p className="font-semibold">{movieInfo.title}</p>
+              </div>
+              <div
+                id="content"
+                className="pl-4 pr-2 lg:pt-10 md:pt-16 sm:pt-24 pt-28  h-full w-4/6   "
+              >
+                <div className="   ">
+                  <p className="text-white md:text-4xl sm:text-3xl text-2xl font-semibold  ">
+                    {movieInfo.title}
+                  </p>
+                </div>
+                <hr className="border my-2 mt-3" />
+                <div className="text-white  flex flex-wrap  ">
+                  {/* <span className="font-semibold">Genres :</span> */}
+                  {movieInfo.genres.map((genre, index) => (
+                    <span
+                      className="md:text-base sm:text-sm text-xs bg-yellow-500 ml-1 rounded-lg p-1 cursor-pointer"
+                      key={genre.id}
+                    >
+                      {genre.name}
+                      {/* {index !== movieInfo.genres.length - 1 && ","} */}
+                    </span>
+                  ))}
+                </div>
+                <div className="w-full mt-2 flex items-center">
+                  <CircularProgressBar percentage={voteAvgInPercentage} />
+                  <div className="mx-1">
+                    <button className="bg-black p-2 border-2 border-white text-white font-medium ">
+                      Play Trailer
+                    </button>
+                  </div>
+                </div>
+
+                <div id="overview" className=" text-white    ">
+                  <p className=" md:text-2xl sm:text-xl text-lg font-bold py-2   ">
+                    Overview
+                  </p>
+                  <p
+                    className={`${
+                      movieInfo.overview.length >= 1380
+                        ? "overflow-y-scroll "
+                        : ""
+                    } lg:h-64 md:h-48 sm:h-40 h-36   md:text-lg sm:text-base text-sm font-normal overflow-auto    `}
+                  >
+                    {movieInfo.overview}
+                  </p>
                 </div>
               </div>
             </div>
