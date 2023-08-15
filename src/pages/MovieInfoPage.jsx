64bd5f406ca4babf59b6bf7712fd1ae5fedc2b27
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-15 13:59:32 +0700

    [UPDATE] prepare merge mobile and desktop  banner section

--- src/pages/MovieInfoPage.jsx
+++ src/pages/MovieInfoPage.jsx
@@ -31,9 +31,7 @@ const MovieInfoPage = ({}) => {
       <>
         <div className=" h-full w-full ">
           <div id="banner-section" className="h-full w-full   ">
-            {isMobileScreen
-              ? renderTitleAndBannerSectionMobile()
-              : renderTitleAndBannerSection()}
+            {renderTitleAndBannerSectionDTAntMB()}
           </div>
 
           <div id="details" className=" ">
@@ -43,7 +41,81 @@ const MovieInfoPage = ({}) => {
       </>
     );
   };
+//------------------------------------
+  const renderTitleAndBannerSectionDTAntMB = () => {
+    return (
+      <>
+        <div className="xl:container mx-auto h-full  w-full flex justify-center ">
+          <div className="relative sm:h-128 h-176  w-full   bg-black ">
+            <div id="image-background" className="h-128 w-full opacity-40 ">
+              <img
+                className="w-full sm:h-full h-176 object-cover"
+                src={`${imageUrl}/${movieInfo.backdrop_path}`}
+                alt="Banner Image"
+              />
+            </div>
+            <div className="absolute top-0 h-full w-full  flex sm:flex-row flex-col   ">
+              <div id="poster" className="  sm:pt-0 pt-2  sm:w-2/6 w-full">
+                <div className="w-full h-full flex justify-center items-center   ">
+                  <img
+                    className="lg:h-112 md:h-96 sm:h-80 h-72 lg:w-76 md:w-64 sm:w-56 w-48 shadow-xl rounded-xl border-4 border-white bg-gray-200"
+                    src={`${imageUrl}/${movieInfo.poster_path}`}
+                  />
+                </div>
+              </div>
+              <div
+                id="content"
+                className="px-2 lg:pt-10 md:pt-16  pt-2  h-full sm:w-4/6 w-full   "
+              >
+                <div className="   ">
+                  <p className="text-white md:text-4xl sm:text-3xl text-2xl font-semibold  ">
+                    {movieInfo.title}
+                  </p>
+                </div>
+                <hr className="border my-2 mt-3" />
+                <div className="text-white  flex flex-wrap  ">
+                  {/* <span className="font-semibold">Genres :</span> */}
+                  {movieInfo.genres.map((genre, index) => (
+                    <span
+                      className="md:text-base sm:text-sm text-xs bg-yellow-500 ml-1 rounded-lg p-1 cursor-pointer"
+                      key={genre.id}
+                    >
+                      {genre.name}
+                      {/* {index !== movieInfo.genres.length - 1 && ","} */}
+                    </span>
+                  ))}
+                </div>
+                <div className="w-full mt-2 flex items-center">
+                  <CircularProgressBar percentage={voteAvgInPercentage} />
+                  <div className="mx-1">
+                    <button className="bg-black p-2 border-2 border-white text-white font-medium ">
+                      Play Trailer
+                    </button>
+                  </div>
+                </div>
 
+                <div id="overview" className=" text-white    ">
+                  <p className=" md:text-2xl sm:text-xl text-lg font-bold py-2   ">
+                    Overview
+                  </p>
+                  <p
+                    className={`${
+                      movieInfo.overview.length >= 1380
+                        ? "overflow-y-scroll "
+                        : ""
+                    } lg:h-64 md:h-48 sm:h-40 h-36   md:text-lg sm:text-base text-sm font-normal overflow-auto    `}
+                  >
+                    {movieInfo.overview}{movieInfo.overview}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+////////////////////////////////////////////////////////////////////////
   const renderTitleAndBannerSection = () => {
     return (
       <>
@@ -162,6 +234,10 @@ const MovieInfoPage = ({}) => {
       </>
     );
   };
+
+
+
+
 
   const renderTitleAndBannerSectionMobile = () => {
     return (
