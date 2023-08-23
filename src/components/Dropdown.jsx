3a99241aea8f36a80315cd234c0c187e852715dc
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-23 19:42:23 +0700

    [UPDATE] finnish drop down

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 
 function Dropdown() {
   return (
@@ -13,12 +14,15 @@ function Dropdown() {
       >
         <div className="w-full    rounded-lg overflow-hidden  ">
           <ul className="   text-center  ">
-            <li className="hover:bg-gray-100 py-2  border-b"> Login</li>
-
             <li className="hover:bg-gray-100 py-2  border-b">
-              Favorite Movie
+              <Link>Login</Link>
             </li>
-            <li className="hover:bg-gray-100  py-2"> Logout</li>
+            <li className="hover:bg-gray-100 py-2  border-b">
+              <Link>Favorite Movie</Link>
+            </li>
+            <li className="hover:bg-gray-100  py-2">
+              <Link>Logout</Link>
+            </li>
           </ul>
         </div>
       </div>
