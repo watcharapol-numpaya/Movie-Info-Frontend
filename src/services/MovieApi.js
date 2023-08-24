49b1f80987028876e3774cc4b0e544ade22d72c7
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-24 13:54:04 +0700

    [UPDATE] using env file

--- src/services/MovieApi.js
+++ src/services/MovieApi.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const instance =  axios.create({
-    baseURL:'https://api.themoviedb.org/3/'
-})
+// export const instance =  axios.create({
+//     baseURL:'https://api.themoviedb.org/3/'
+// })
 
- 
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_API,
+});
