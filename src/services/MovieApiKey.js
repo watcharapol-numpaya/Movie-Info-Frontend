ecafee3f526448bfe803ecfeac62081949e20e02
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-24 13:54:51 +0700

    [DELTE] delete api commnet

--- src/services/MovieApiKey.js
+++ src/services/MovieApiKey.js
@@ -1,3 +1,1 @@
-// export const APIKey ="4232f2de"
-// export const APIKeyTMDB = "64a9ab678775a4e88a16d347aa49fc60"
-export const APIKeyTMDB = import.meta.env.VITE_API_KEY;
+ export const APIKeyTMDB = import.meta.env.VITE_API_KEY;
