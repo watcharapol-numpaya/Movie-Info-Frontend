f76d54aaa5a3b8b4510aadace3fe993dcf19566f
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-08-27 12:55:58 +0700

    [FIX] fix drop-down

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,33 +3,30 @@ import { Link } from "react-router-dom";
 
 function Dropdown() {
   return (
-    <>
-      <div
-        id="drop-down"
-        className="absolute z-10 bg-white h-8 w-8 right-2 top-14  rotate-45 "
-      ></div>
-      <div
-        id="drop-down"
-        className="absolute z-20 bg-white  h-auto w-52 right-0 top-16 rounded-lg  shadow-2xl"
-      >
-        <div className="w-full    rounded-lg overflow-hidden  ">
-          <ul className="   text-center  ">
-            <li className="hover:bg-gray-100 py-2  border-b">
-              <Link>Sign Up</Link>
-            </li>
-            <li className="hover:bg-gray-100 py-2  border-b">
-              <Link to="/sign-in">Sign In</Link>
-            </li>
-            <li className="hover:bg-gray-100 py-2  border-b">
-              <Link>Favorite Movie</Link>
-            </li>
-            <li className="hover:bg-gray-100  py-2">
-              <Link>Sign Out</Link>
-            </li>
-          </ul>
+    <div className="relative">
+      <div className="absolute z-10 bg-white h-8 w-8 right-2 top-2 transform rotate-45"></div>
+      <div className="absolute z-20 bg-white h-auto w-52 right-0 top-4 rounded-lg shadow-2xl">
+        <div className="w-full rounded-lg overflow-hidden">
+          <div className="text-center  font-medium flex flex-col">
+            <Link className="hover:bg-gray-100 py-2 border-b" to="/sign-up">
+              Sign Up
+            </Link>
+            <Link className="hover:bg-gray-100 py-2 border-b" to="/sign-in">
+              Sign In
+            </Link>
+            <Link
+              className="hover:bg-gray-100 py-2 border-b"
+              to="/favorite-movie"
+            >
+              Favorite Movie
+            </Link>
+            <Link className="hover:bg-gray-100 py-2 border-b" to="/sign-out">
+              Sign Out
+            </Link>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
