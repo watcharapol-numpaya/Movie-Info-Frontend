456bef86b2340635ef007765333b84497dcb4c7e
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-09-03 21:57:39 +0700

    [DELETE] delete mApi

--- src/services/mApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { checkTokenExpiration } from "./jwtTokenService";
-
-export const movieApiInstance = axios.create({
-  baseURL: import.meta.env.VITE_API,
-});
-
- 
