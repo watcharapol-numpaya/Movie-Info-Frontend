22c6ed276f11db604679d76d511516ecf6885a97
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-09-03 19:14:51 +0700

    [UPDATE] add relaod page when sign out

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -11,12 +11,13 @@ function Dropdown() {
   const handleSignOut = () => {
     dispatch(logout());
     dispatch(clearUserSliceState());
+    location.reload()
   };
 
   return (
     <div className="relative">
-      <div className="absolute z-10 bg-white h-8 w-8 -right-1 top-8 transform rotate-45"></div>
-      <div className="absolute z-20 bg-white h-auto w-52 -right-3 top-10 rounded-lg shadow-2xl">
+      <div className="absolute z-10 bg-white h-8 w-8 -right-0 top-8 transform rotate-45"></div>
+      <div className="absolute z-20 bg-white h-auto w-52 -right-2 top-10 rounded-lg shadow-2xl">
         <div className="w-full rounded-lg overflow-hidden">
           <div className="text-center  font-medium flex flex-col">
             <Link
