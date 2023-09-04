d4ee0c9bbc34aed68b0596ae7605f11b54a76377
watcharapol numpaya <watcharapol.num@mail.kmutt.ac.th>
2023-09-04 22:33:59 +0700

    [UPDATE] covert ViewAllSearchResultPage to component

--- src/pages/ViewAllSearchResultPage.jsx
+++ src/pages/ViewAllSearchResultPage.jsx
@@ -6,75 +6,23 @@ import { getMovieByKeyword } from "../storage/slices/movieSlice";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import MovieCard from "../components/MovieCard";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MovieListCard from "../components/MovieListCard";
 
 const ViewAllSearchResultPage = () => {
   const [isLoading, setIsLoading] = useState(true);
-  // const dispatch = useDispatch();
   const { searchList } = useSelector((state) => state.movies); //receive data from search
   const { keyword } = useParams();
 
   useEffect(() => {
-    // dispatch(getMovieByKeyword(keyword));
+ 
     if (searchList !== []) {
       setIsLoading(false);
     }
   }, []);
-
-  const renderNotFound = () => {
-    return (
-      <div className="  h-144 flex   items-center ">
-        <p className="text-black text-2xl font-semibold">
-          Not Found : {searchList.length === 0 ? keyword : ""}
-        </p>
-      </div>
-    );
-  };
-
-  const renderShowMovie = () => {
-    return (
-      <>
-        {searchList.length !== 0 ? (
-          <>
-            {searchList &&
-              searchList.map((item) => (
-                <div key={item.id}>
-                  <MovieCard movie={item} />
-                </div>
-              ))}
-          </>
-        ) : (
-          <>{renderNotFound()}</>
-        )}
-      </>
-    );
-  };
-
-  const renderViewAllResult = () => {
-    return (
-      <div className="xl:container mx-auto ">
-        <div className="w-full bg-red">
-          <div className="flex  p-4 text-black sm:mb-4  ">
-            <Link className=" flex  items-center  " to="/">
-              <span className="sm:block flex">
-                <ArrowBackIosIcon />
-              </span>
-            </Link>
-            <span id="Back" className="font-semibold text-2xl  uppercase ">
-              Result
-            </span>
-          </div>
-          <div className="w-full flex justify-center ">
-            <div className="flex  flex-wrap xl:gap-6 gap-4 justify-center mb-4  ">
-              {renderShowMovie()}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
+ 
   return (
     <div className=" ">
-      {isLoading ? <OnLoadingScreen /> : renderViewAllResult()}
+      {isLoading ? <OnLoadingScreen /> : <MovieListCard movies={searchList} title="Result" notFoundTitle={keyword} />}
     </div>
   );
 };
